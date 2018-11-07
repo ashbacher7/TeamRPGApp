53b43b479dfae6a2462841ac5f5090cb831ca91e
0767373 <0767373@KELCE-ACIS22-P>
2018-11-07 17:44:16 -0600

    Added 2 Choice pages, 2 End pages.

---
+++ TeamRPGApp/TeamRPGApp/ChoiceB.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TeamRPGApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ChoiceB : ContentPage
+	{
+		public ChoiceB ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
