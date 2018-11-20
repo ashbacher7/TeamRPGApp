a5d29502923b3f4998a269857d69e70f19d0cf9c
0767373 <0767373@KELCE-ACIS22-P>
2018-11-19 18:00:51 -0600

    Set up bools for all 9 viewend buttons, need to finish the code behind on the ending pages and ViewEnd page.

--- TeamRPGApp/TeamRPGApp/Globals.cs
+++ TeamRPGApp/TeamRPGApp/Globals.cs
@@ -7,5 +7,13 @@ namespace TeamRPGApp
     public static class Globals
     {
         public static bool AndroidGoodEndShow = false;
+        public static bool AndroidBadEndShow = false;
+        public static bool AndroidSelfishEndShow = false;
+        public static bool CRGoodEndShow = false;
+        public static bool CRBadEndShow = false;
+        public static bool CRSelfishEndShow = false;
+        public static bool SKGoodEndShow = false;
+        public static bool SKBadEndShow = false;
+        public static bool SKSelfishEndShow = false;
     }
 }
