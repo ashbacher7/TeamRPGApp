73f58fb6b8158bc94156639442d9bef9176cc590
0458808 <0458808@KELCE-ACIS14-P>
2018-12-05 17:36:18 -0600

    Changed naming scheme and started working on Android story

---
+++ TeamRPGApp/TeamRPGApp/1Android/1-4Android.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TeamRPGApp._1Android
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class 1-4Android : ContentPage
+	{
+		public 1-4Android ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
