d3370446f18d59b09138961f755ebc0d0ed0ce82
0767373 <0767373@KELCE-ACIS22-P>
2018-12-10 17:38:16 -0600

    Moved intro3 button to the bottom to be sure it's visible in emulator.

--- TeamRPGApp/TeamRPGApp.Android/Resources/Resource.designer.cs
+++ TeamRPGApp/TeamRPGApp.Android/Resources/Resource.designer.cs
@@ -2342,47 +2342,11 @@ namespace TeamRPGApp.Droid
 			// aapt resource value: 0x7f020053
 			public const int Android12choiceA1 = 2130837587;
 			
-			// aapt resource value: 0x7f020132
-			public const int avd_hide_password_1 = 2130837810;
-			
-			// aapt resource value: 0x7f020133
-			public const int avd_hide_password_2 = 2130837811;
-			
-			// aapt resource value: 0x7f020134
-			public const int avd_hide_password_3 = 2130837812;
-			
 			// aapt resource value: 0x7f020054
 			public const int avd_hide_password = 2130837588;
 			
-			// aapt resource value: 0x7f02013b
-			public const int avd_hide_password_1 = 2130837819;
-			
-			// aapt resource value: 0x7f02013c
-			public const int avd_hide_password_2 = 2130837820;
-			
-			// aapt resource value: 0x7f02013d
-			public const int avd_hide_password_3 = 2130837821;
-			
-			// aapt resource value: 0x7f020135
-			public const int avd_show_password_1 = 2130837813;
-			
-			// aapt resource value: 0x7f020136
-			public const int avd_show_password_2 = 2130837814;
-			
-			// aapt resource value: 0x7f020137
-			public const int avd_show_password_3 = 2130837815;
-			
 			// aapt resource value: 0x7f020055
 			public const int avd_show_password = 2130837589;
-			
-			// aapt resource value: 0x7f02013e
-			public const int avd_show_password_1 = 2130837822;
-			
-			// aapt resource value: 0x7f02013f
-			public const int avd_show_password_2 = 2130837823;
-			
-			// aapt resource value: 0x7f020140
-			public const int avd_show_password_3 = 2130837824;
 			
 			// aapt resource value: 0x7f020056
 			public const int backarrow = 2130837590;
